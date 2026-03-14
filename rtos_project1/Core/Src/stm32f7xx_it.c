eae0eff731a59d6a8f096921b064e22dad054e45
Mohammad Arafath Uz Zaman <zaman.arafath@gmail.com>
2026-03-14 12:54:19 +0100

    feat: Use TIM6 as HAL timebase

--- rtos_project1/Core/Src/stm32f7xx_it.c
+++ rtos_project1/Core/Src/stm32f7xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim6;
 
 /* USER CODE BEGIN EV */
 
@@ -138,16 +139,16 @@ void UsageFault_Handler(void)
   }
 }
 
-///**
-//  * @brief This function handles System service call via SWI instruction.
-//  */
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
 //void SVC_Handler(void)
 //{
 //  /* USER CODE BEGIN SVCall_IRQn 0 */
-//
+////
 //  /* USER CODE END SVCall_IRQn 0 */
 //  /* USER CODE BEGIN SVCall_IRQn 1 */
-//
+////
 //  /* USER CODE END SVCall_IRQn 1 */
 //}
 
@@ -164,30 +165,30 @@ void DebugMon_Handler(void)
   /* USER CODE END DebugMonitor_IRQn 1 */
 }
 
-///**
-//  * @brief This function handles Pendable request for system service.
-//  */
+/**
+  * @brief This function handles Pendable request for system service.
+  */
 //void PendSV_Handler(void)
 //{
 //  /* USER CODE BEGIN PendSV_IRQn 0 */
-//
+////
 //  /* USER CODE END PendSV_IRQn 0 */
 //  /* USER CODE BEGIN PendSV_IRQn 1 */
-//
+////
 //  /* USER CODE END PendSV_IRQn 1 */
 //}
 
-///**
-//  * @brief This function handles System tick timer.
-//  */
+/**
+  * @brief This function handles System tick timer.
+  */
 //void SysTick_Handler(void)
 //{
 //  /* USER CODE BEGIN SysTick_IRQn 0 */
-//
+////
 //  /* USER CODE END SysTick_IRQn 0 */
-//  HAL_IncTick();
-//  /* USER CODE BEGIN SysTick_IRQn 1 */
 //
+//  /* USER CODE BEGIN SysTick_IRQn 1 */
+////
 //  /* USER CODE END SysTick_IRQn 1 */
 //}
 
@@ -197,6 +198,20 @@ void DebugMon_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f7xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles TIM6 global interrupt, DAC1 and DAC2 underrun error interrupts.
+  */
+void TIM6_DAC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+  /* USER CODE END TIM6_DAC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim6);
+  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+
+  /* USER CODE END TIM6_DAC_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
